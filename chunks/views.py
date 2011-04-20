4d494af7b17440a810338d10ce2244d9ca411d03
Mason Tang <masont@funkytown.xvm.mit.edu>
2011-04-20 16:21:44 -0400

    cleaned up chunk display

--- chunks/views.py
+++ chunks/views.py
@@ -3,6 +3,9 @@ from caesar.chunks.models import Chunk, File
 from django.http import Http404
 from django.shortcuts import render_to_response
 
+import textwrap
+import string
+
 def view_chunk(request, chunk_id):
     try:
         chunk = Chunk.objects.get(pk=chunk_id)
@@ -11,7 +14,8 @@ def view_chunk(request, chunk_id):
         first_line_offset = chunk.start
         while file_data[first_line_offset] != '\n':
             first_line_offset -= 1
-        data = file_data[first_line_offset + 1:chunk.end]
+        data = textwrap.dedent(file_data[first_line_offset + 1:chunk.end])
+        data = data.expandtabs(4)
     except Chunk.DoesNotExist:
         raise Http404
     return render_to_response('chunks/view_chunk.html', { 
