d847e4f9c0a5f817c0dded0c103ce8a4b97052cf
Mason Tang <masont@mit.edu>
2011-05-24 14:00:21 -0400

    Cleaned up fabfile to be more in line with newer Fabric APIs

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,13 @@
 from fabric.api import *
 
 env.project_name = 'caesar'
+env.project_path = '/var/django/caesar'
 
-def prod():
-    env.hosts = ['caesar.xvm.mit.edu']
-    env.path = '/var/django/caesar'
+env.roledefs = {
+    'prod': ['caesar.xvm.mit.edu'],
+}
+
+env.roles = ['prod']
 
 def deploy():
     """
@@ -15,13 +18,13 @@ def deploy():
     restart_webserver()
 
 def update_code():
-    with cd(env.path):
+    with cd(env.project_path):
         run('git pull')
 
 def install_project():
     # symlink the caesar apache configuration file to apache
-    sudo('cd /etc/apache2/sites-enabled; ln -sf %(path)s/apache/%(project_name)s %(project_name)s' % env)
-    with cd(env.path):
+    sudo('cd /etc/apache2/sites-enabled; ln -sf %(project_path)s/apache/%(project_name)s %(project_name)s' % env)
+    with cd(env.project_path):
         run('python manage.py collectstatic --noinput')
         run('python manage.py syncdb --noinput')
         run('python manage.py migrate')
