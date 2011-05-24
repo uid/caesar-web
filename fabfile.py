c7ee76961ea6ab0ed1952c78a4ffe0149a67dd4e
Mason Tang <masont@mit.edu>
2011-05-23 22:35:41 -0400

    Made a basic fabfile to deploy to prod

---
+++ fabfile.py
@@ -0,0 +1,32 @@
+from fabric.api import *
+
+env.project_name = 'caesar'
+
+def prod():
+    env.hosts = ['caesar.xvm.mit.edu']
+    env.path = '/var/django/caesar'
+    env.user = 'masont'
+
+def deploy():
+    """
+    Pulls the latest code from git and deploys it.
+    """
+    update_code()
+    install_project()
+    restart_webserver()
+
+def update_code():
+    with cd(env.path):
+        run('git pull')
+
+def install_project():
+    # symlink the caesar apache configuration file to apache
+    sudo('cd /etc/apache2/sites-enabled; ln -sf %(path)s/apache/%(project_name)s %(project_name)s' % env)
+    with cd(env.path):
+        run('python manage.py collectstatic')
+        run('python manage.py syncdb --noinput')
+        run('python manage.py migrate')
+
+def restart_webserver():
+    sudo('service apache2 restart')
+
