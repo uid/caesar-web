5080610e4d9aad1a1de2bc356c2a315b20a3088e
Mason Tang <masont@mit.edu>
2011-06-02 17:43:44 -0400

    Added some polish to comment display

--- chunks/views.py
+++ chunks/views.py
@@ -11,38 +11,40 @@ import string
 
 @login_required
 def view_chunk(request, chunk_id):
-	try:
-		chunk = Chunk.objects.get(pk=chunk_id)
-		file_data = chunk.file.data
-		# Rewind backwards from the offset to the beginning of the line
-		first_line_offset = chunk.start
-		while file_data[first_line_offset] != '\n':
-			first_line_offset -= 1
-		first_line_offset += 1
+    try:
+        chunk = Chunk.objects.get(pk=chunk_id)
+        file_data = chunk.file.data
+        # Rewind backwards from the offset to the beginning of the line
+        first_line_offset = chunk.start
+        while file_data[first_line_offset] != '\n':
+            first_line_offset -= 1
+        first_line_offset += 1
 
-		first_line = file_data.count("\n", 0, first_line_offset)+1
+        first_line = file_data.count("\n", 0, first_line_offset)+1
 
-		# TODO: make tab expansion configurable
-		# TODO: more robust (custom) dedenting code
-		data = file_data[first_line_offset:chunk.end].expandtabs(4)
-		lines = enumerate(textwrap.dedent(data).splitlines(), start=first_line)
+        # TODO: make tab expansion configurable
+        # TODO: more robust (custom) dedenting code
+        data = file_data[first_line_offset:chunk.end].expandtabs(4)
+        lines = list(enumerate(textwrap.dedent(data).splitlines(), 
+            start=first_line))
 
-		def get_comment_vote(comment):
-			try:
-				vote = comment.votes.get(author=request.user.id).value
-			except Vote.DoesNotExist:
-				vote = None
-			return (comment, vote)
+        def get_comment_data(comment):
+            try:
+                vote = comment.votes.get(author=request.user.id).value
+            except Vote.DoesNotExist:
+                vote = None
+            snippet = lines[comment.start - first_line][1]
+            return (comment, vote, snippet)
 
-		comment_data = map(get_comment_vote, Comment.get_comments_for_chunk(chunk))
-		
-		#get the star data, or create it if it doesn't exist
-		star = Star.objects.get_or_create(author=request.user,chunk=chunk)
-	except Chunk.DoesNotExist:
-		raise Http404
-	return render_to_response('chunks/view_chunk.html', { 
-		'chunk': chunk,
-		'lines': lines,
-		'comment_data': comment_data,
-		'star':star,
-	}, context_instance=RequestContext(request)) 
+        comment_data = map(get_comment_data, Comment.get_comments_for_chunk(chunk))
+        
+        #get the star data, or create it if it doesn't exist
+        star = Star.objects.get_or_create(author=request.user,chunk=chunk)
+    except Chunk.DoesNotExist:
+        raise Http404
+    return render_to_response('chunks/view_chunk.html', { 
+        'chunk': chunk,
+        'lines': lines,
+        'comment_data': comment_data,
+        'star':star,
+    }, context_instance=RequestContext(request)) 
