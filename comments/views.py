adeed7edfac9294a573fdfbf886b2fbce0845aae
Mason Tang <masont@mit.edu>
2011-06-03 18:07:15 -0400

    Visual polish for new comment form

--- comments/views.py
+++ comments/views.py
@@ -10,83 +10,93 @@ from django.http import HttpResponse
 
 @login_required
 def new(request):
-	if request.method == 'GET':
-		form = CommentForm(initial={
-			'start': request.GET['start'],
-			'end': request.GET['end'],
-			'chunk': request.GET['chunk']
-		})
-		return render(request, 'comments/comment_form.html', {'form': form,})	
-	else:
-		form = CommentForm(request.POST)
-		if form.is_valid():
-			comment = form.save(commit=False)
-			comment.author = request.user
-			comment.save()
-			return redirect(comment.chunk)
+    if request.method == 'GET':
+        start = int(request.GET['start'])
+        end = int(request.GET['end'])
+        chunk_id = request.GET['chunk']
+        form = CommentForm(initial={
+            'start': start,
+            'end': end,
+            'chunk': chunk_id
+        })
+        chunk = Chunk.objects.get(pk=chunk_id)
+        return render(request, 'comments/comment_form.html', {
+            'form': form,
+            'start': start,
+            'end': end,
+            'snippet': chunk.generate_snippet(start, end),
+            'chunk': chunk,
+        })  
+    else:
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.save()
+            return redirect(comment.chunk)
 
 @login_required
 def reply(request):
-	if request.method == 'GET':
-		form = ReplyForm(initial={
-			'parent': request.GET['parent']
-		})
-		return render(request, 'comments/reply_form.html', {'form': form,})
-	else:
-		form = ReplyForm(request.POST)
-		if form.is_valid():
-			comment = form.save(commit=False)
-			comment.author = request.user
-			parent = Comment.objects.get(id=comment.parent.id)
-			comment.chunk = parent.chunk
-			comment.end = parent.end
-			comment.start = parent.start 
-			comment.save()
-			return redirect(comment.chunk)
+    if request.method == 'GET':
+        form = ReplyForm(initial={
+            'parent': request.GET['parent']
+        })
+        return render(request, 'comments/reply_form.html', {'form': form,})
+    else:
+        form = ReplyForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            parent = Comment.objects.get(id=comment.parent.id)
+            comment.chunk = parent.chunk
+            comment.end = parent.end
+            comment.start = parent.start 
+            comment.save()
+            return redirect(comment.chunk)
 
 @login_required
 def delete(request):
-	comment_id = request.GET['comment_id']
-	comment = Comment.objects.get(pk=comment_id)
-	if comment.author == request.user:
-		comment.delete()
-	return HttpResponse('deleted')
+    comment_id = request.GET['comment_id']
+    comment = Comment.objects.get(pk=comment_id)
+    if comment.author == request.user:
+        comment.delete()
+    return HttpResponse('deleted')
 
 @login_required
 def vote(request):
-	comment_id = request.POST['comment_id']
-	value = request.POST['value']
-	comment = Comment.objects.get(pk=comment_id)
-	try:
-		vote = Vote.objects.get(comment=comment, author=request.user)
-		vote.value = value
-	except Vote.DoesNotExist:
-		vote = Vote(comment=comment, value=value, author=request.user)
+    comment_id = request.POST['comment_id']
+    value = request.POST['value']
+    comment = Comment.objects.get(pk=comment_id)
+    try:
+        vote = Vote.objects.get(comment=comment, author=request.user)
+        vote.value = value
+    except Vote.DoesNotExist:
+        vote = Vote(comment=comment, value=value, author=request.user)
 
-	vote.save()
-	return render(request, 'comments/comment_votes.html', {'comment': comment})
+    vote.save()
+    return render(request, 'comments/comment_votes.html', {'comment': comment})
 
 @login_required
 def unvote(request):
-	comment_id = request.POST['comment_id']
-	comment = Comment.objects.get(pk=comment_id)
-	Vote.objects.filter(comment=comment, author=request.user).delete()
-	return render(request, 'comments/comment_votes.html', {'comment': comment})
+    comment_id = request.POST['comment_id']
+    comment = Comment.objects.get(pk=comment_id)
+    Vote.objects.filter(comment=comment, author=request.user).delete()
+    return render(request, 'comments/comment_votes.html', {'comment': comment})
 
 @login_required
 def change_star(request):
-	if request.POST['value'] == "check":
-		value = True
-	else:
-		value = False
-	chunk_id = request.POST['chunk_id']
-	chunk = Chunk.objects.get(pk=chunk_id)
-	try:
-		star = Star.objects.get(chunk=chunk, author=request.user)
-		star.value = value
-		star.save()
-	except Star.DoesNotExist:
-		star = Star(chunk=chunk, value=value, author=request.user)
-		star.save()
-		
-	return render(request,'comments/change_star.html',{'star':star})
+    if request.POST['value'] == "check":
+        value = True
+    else:
+        value = False
+    chunk_id = request.POST['chunk_id']
+    chunk = Chunk.objects.get(pk=chunk_id)
+    try:
+        star = Star.objects.get(chunk=chunk, author=request.user)
+        star.value = value
+        star.save()
+    except Star.DoesNotExist:
+        star = Star(chunk=chunk, value=value, author=request.user)
+        star.save()
+        
+    return render(request,'comments/change_star.html',{'star':star})
--- utils/templatetags/extra_tags.py
+++ utils/templatetags/extra_tags.py
@@ -11,21 +11,21 @@ def humanize_timesince(date):
 
     num_years = delta.days / 365
     if (num_years > 0):
-        return ungettext(u"%d year ago", u"%d years ago", num_years) % num_years
+        return ungettext(u"%d year", u"%d years", num_years) % num_years
 
     num_weeks = delta.days / 7
     if (num_weeks > 0):
-        return ungettext(u"%d week ago", u"%d weeks ago", num_weeks) % num_weeks
+        return ungettext(u"%d week", u"%d weeks", num_weeks) % num_weeks
 
     if (delta.days > 0):
-        return ungettext(u"%d day ago", u"%d days ago", delta.days) % delta.days
+        return ungettext(u"%d day", u"%d days", delta.days) % delta.days
 
     num_hours = delta.seconds / 3600
     if (num_hours > 0):
-        return ungettext(u"%d hour ago", u"%d hours ago", num_hours) % num_hours
+        return ungettext(u"%d hour", u"%d hours", num_hours) % num_hours
 
     num_minutes = delta.seconds / 60
     if (num_minutes > 0):
-        return ungettext(u"%d minute ago", u"%d minutes ago", num_minutes) % num_minutes
+        return ungettext(u"%d minute", u"%d minutes", num_minutes) % num_minutes
 
-    return ugettext(u"just a few seconds ago")
+    return ugettext(u"just a few seconds")
