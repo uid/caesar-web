7e73d2a707c6eaf4e6de90b3ffb357422364311f
Mason Tang <masont@mit.edu>
2011-06-13 16:07:42 -0400

    Made comment text form fields shorter.

--- review/forms.py
+++ review/forms.py
@@ -8,14 +8,17 @@ class CommentForm(ModelForm):
         model = Comment
         fields = ('text','start', 'end', 'chunk')
         widgets = {
+            'text': Textarea(attrs={'cols': 10, 'rows': 5}), 
             'start': HiddenInput(),
             'end': HiddenInput(),
             'chunk': HiddenInput(),
         }
+
 class ReplyForm(ModelForm):
 	class Meta:
 		model = Comment
 		fields = ('text', 'parent')
 		widgets = {
+            'text': Textarea(attrs={'cols': 10, 'rows': 5}), 
 			'parent': HiddenInput(),
 		}
