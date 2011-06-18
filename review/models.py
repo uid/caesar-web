uid/caesar-web

commit 3e088b4aeff687e72ac0d1fd8e2e00bdb3b45b74

Added a thread_id field to comment to save some queries
The additional field lets us retrieve comments in threaded order in a
single query. Also modified comment collapsing behavior to include child
comments.

The parent_id field now will always point to the root comment, unless
the comment is not a reply, in which case it is still null.

review/models.py
@@ -25,9 +25,16 @@ class Comment(models.Model):
     # fields added for denormalization purposes
     upvote_count = models.IntegerField(default=0)
     downvote_count = models.IntegerField(default=0)
+    # Set to either self.id for root comments or parent.id for replies, mostly
+    # to allow for retrieving comments in threaded order in one query
+    thread_id = models.IntegerField()
 
     def __unicode__(self):
         return self.text
+
+    def save(self, *args, **kwargs):
+        self.thread_id = self.parent_id or self.id
+        super(Comment, self).save(*args, **kwargs)
 
     #returns child and vote counts for child as a tuple
     def get_child_comment_vote(self):
@@ -41,27 +48,14 @@ class Comment(models.Model):
         return (self, vote)
 
     def is_reply(self):
-        if self.parent:
-            return True
-        else:
-            return False
+        return self.parent_id is not None
             
     @staticmethod
     def get_comments_for_chunk(chunk):
-        all_comments = []
-        parent_comments= \
-            Comment.objects.select_related('author') \
-                   .filter(chunk=chunk).filter(parent=None)
-        for parent_comment in parent_comments:
-            temp_stack = [parent_comment]
-            while len(temp_stack) != 0:
-                temp_child = temp_stack.pop(0)
-                all_comments.append(temp_child)
-                temp_stack = list(temp_child.child_comments.all()) + temp_stack
-        return all_comments
+        return chunk.comments.select_related('author')
 
     class Meta:
-        ordering = [ 'start', '-end' ]
+        ordering = [ 'start', '-end', 'thread_id', 'created' ]
 
 class Vote(models.Model):
     VALUE_CHOICES = (

review/views.py
@@ -69,6 +69,9 @@ def reply(request):
             comment.chunk = parent.chunk
             comment.end = parent.end
             comment.start = parent.start 
+            if parent.is_reply():
+                # strictly single threads for discussion
+                comment.parent = parent.parent
             comment.save()
             return redirect(comment.chunk)
 

templates/chunks/view_chunk.html
@@ -45,10 +45,14 @@ function showCommentForm(startLine, endLine) {
 function collapseComment(comment) {
     $(comment.elt).removeClass('expanded')
             .addClass('collapsed', 'fast');
+    $('.parent-' + comment.id).removeClass('expanded')
+            .addClass('collapsed', 'fast');
 };
 
 function expandComment(comment) {
     $(comment.elt).removeClass('collapsed', 'fast')
+            .addClass('expanded');
+    $('.parent-' + comment.id).removeClass('collapsed', 'fast')
             .addClass('expanded');
 };
 

review/migrations/0007_auto__add_field_comment_thread_id.py
@@ -1,0 +1,139 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding field 'Comment.thread_id'
+        db.add_column('review_comment', 'thread_id', self.gf('django.db.models.fields.IntegerField')(default=1), keep_default=False)
+
+
+    def backwards(self, orm):
+        
+        # Deleting field 'Comment.thread_id'
+        db.delete_column('review_comment', 'thread_id')
+
+
+    models = {
+        'accounts.userprofile': {
+            'Meta': {'object_name': 'UserProfile'},
+            'assigned_chunks': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'reviewers'", 'symmetrical': 'False', 'through': "orm['review.Task']", 'to': "orm['chunks.Chunk']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'photo': ('sorl.thumbnail.fields.ImageField', [], {'max_length': '100'}),
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['auth.User']", 'unique': 'True'})
+        },
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'chunks.assignment': {
+            'Meta': {'object_name': 'Assignment', 'db_table': "u'assignments'"},
+            'created': ('django.db.models.fields.DateTimeField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'chunks.chunk': {
+            'Meta': {'object_name': 'Chunk', 'db_table': "u'chunks'"},
+            'created': ('django.db.models.fields.DateTimeField', [], {}),
+            'end': ('django.db.models.fields.IntegerField', [], {}),
+            'file': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['chunks.File']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'start': ('django.db.models.fields.IntegerField', [], {})
+        },
+        'chunks.file': {
+            'Meta': {'unique_together': "(('path', 'submission'),)", 'object_name': 'File', 'db_table': "u'files'"},
+            'created': ('django.db.models.fields.DateTimeField', [], {}),
+            'data': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'path': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'submission': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['chunks.Submission']"})
+        },
+        'chunks.submission': {
+            'Meta': {'object_name': 'Submission', 'db_table': "u'submissions'"},
+            'assignment': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['chunks.Assignment']"}),
+            'created': ('django.db.models.fields.DateTimeField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'review.comment': {
+            'Meta': {'ordering': "['start', '-end', 'thread_id', 'created']", 'object_name': 'Comment'},
+            'author': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
+            'chunk': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'comments'", 'to': "orm['chunks.Chunk']"}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'downvote_count': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'end': ('django.db.models.fields.IntegerField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'child_comments'", 'null': 'True', 'to': "orm['review.Comment']"}),
+            'start': ('django.db.models.fields.IntegerField', [], {}),
+            'text': ('django.db.models.fields.TextField', [], {}),
+            'thread_id': ('django.db.models.fields.IntegerField', [], {}),
+            'type': ('django.db.models.fields.CharField', [], {'default': "'U'", 'max_length': '1'}),
+            'upvote_count': ('django.db.models.fields.IntegerField', [], {'default': '0'})
+        },
+        'review.star': {
+            'Meta': {'object_name': 'Star'},
+            'author': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'stars'", 'to': "orm['auth.User']"}),
+            'chunk': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'stars'", 'to': "orm['chunks.Chunk']"}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'value': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        },
+        'review.task': {
+            'Meta': {'unique_together': "(('chunk', 'reviewer'),)", 'object_name': 'Task'},
+            'chunk': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'tasks'", 'to': "orm['chunks.Chunk']"}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'due': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'reviewer': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'tasks'", 'to': "orm['accounts.UserProfile']"}),
+            'status': ('django.db.models.fields.CharField', [], {'default': "'N'", 'max_length': '1'})
+        },
+        'review.vote': {
+            'Meta': {'unique_together': "(('comment', 'author'),)", 'object_name': 'Vote'},
+            'author': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'votes'", 'to': "orm['auth.User']"}),
+            'comment': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'votes'", 'to': "orm['review.Comment']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'value': ('django.db.models.fields.SmallIntegerField', [], {})
+        }
+    }
+
+    complete_apps = ['review']

review/migrations/0008_add_thread_id.py
@@ -1,0 +1,135 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import DataMigration
+from django.db import models
+from django.db.models import F
+
+class Migration(DataMigration):
+
+    def forwards(self, orm):
+        orm.Comment.objects.filter(parent=None).update(thread_id=F('id'))
+        orm.Comment.objects.exclude(parent=None).update(thread_id=F('parent'))
+
+    def backwards(self, orm):
+        pass
+
+    models = {
+        'accounts.userprofile': {
+            'Meta': {'object_name': 'UserProfile'},
+            'assigned_chunks': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'reviewers'", 'symmetrical': 'False', 'through': "orm['review.Task']", 'to': "orm['chunks.Chunk']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'photo': ('sorl.thumbnail.fields.ImageField', [], {'max_length': '100'}),
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['auth.User']", 'unique': 'True'})
+        },
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'chunks.assignment': {
+            'Meta': {'object_name': 'Assignment', 'db_table': "u'assignments'"},
+            'created': ('django.db.models.fields.DateTimeField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'chunks.chunk': {
+            'Meta': {'object_name': 'Chunk', 'db_table': "u'chunks'"},
+            'created': ('django.db.models.fields.DateTimeField', [], {}),
+            'end': ('django.db.models.fields.IntegerField', [], {}),
+            'file': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['chunks.File']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'start': ('django.db.models.fields.IntegerField', [], {})
+        },
+        'chunks.file': {
+            'Meta': {'unique_together': "(('path', 'submission'),)", 'object_name': 'File', 'db_table': "u'files'"},
+            'created': ('django.db.models.fields.DateTimeField', [], {}),
+            'data': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'path': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'submission': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['chunks.Submission']"})
+        },
+        'chunks.submission': {
+            'Meta': {'object_name': 'Submission', 'db_table': "u'submissions'"},
+            'assignment': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['chunks.Assignment']"}),
+            'created': ('django.db.models.fields.DateTimeField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'review.comment': {
+            'Meta': {'ordering': "['start', '-end', 'thread_id', 'created']", 'object_name': 'Comment'},
+            'author': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
+            'chunk': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'comments'", 'to': "orm['chunks.Chunk']"}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'downvote_count': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'end': ('django.db.models.fields.IntegerField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'child_comments'", 'null': 'True', 'to': "orm['review.Comment']"}),
+            'start': ('django.db.models.fields.IntegerField', [], {}),
+            'text': ('django.db.models.fields.TextField', [], {}),
+            'thread_id': ('django.db.models.fields.IntegerField', [], {}),
+            'type': ('django.db.models.fields.CharField', [], {'default': "'U'", 'max_length': '1'}),
+            'upvote_count': ('django.db.models.fields.IntegerField', [], {'default': '0'})
+        },
+        'review.star': {
+            'Meta': {'object_name': 'Star'},
+            'author': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'stars'", 'to': "orm['auth.User']"}),
+            'chunk': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'stars'", 'to': "orm['chunks.Chunk']"}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'value': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        },
+        'review.task': {
+            'Meta': {'unique_together': "(('chunk', 'reviewer'),)", 'object_name': 'Task'},
+            'chunk': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'tasks'", 'to': "orm['chunks.Chunk']"}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'due': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'reviewer': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'tasks'", 'to': "orm['accounts.UserProfile']"}),
+            'status': ('django.db.models.fields.CharField', [], {'default': "'N'", 'max_length': '1'})
+        },
+        'review.vote': {
+            'Meta': {'unique_together': "(('comment', 'author'),)", 'object_name': 'Vote'},
+            'author': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'votes'", 'to': "orm['auth.User']"}),
+            'comment': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'votes'", 'to': "orm['review.Comment']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'value': ('django.db.models.fields.SmallIntegerField', [], {})
+        }
+    }
+
+    complete_apps = ['review']

review/static/css/comments.css
@@ -15,6 +15,10 @@
     height: 16px;
     overflow: hidden;
 }
+.collapsed.comment.comment-reply {
+    height: 0px; /* this is here to help with the animation */
+    display: none;
+}
 
 .comment.comment-reply {
     border-top: 0px;

templates/review/comment.html
@@ -2,7 +2,11 @@
 
 <!-- comment node ids are named like comment-$ID-$START-$END -->
 <div id="comment-{{ comment.id }}-{{ comment.start }}-{{ comment.end }}" 
-  class="comment {% if comment.is_reply %}comment-reply{% endif %}
+  class="comment 
+         {% if comment.is_reply %}
+           comment-reply 
+           parent-{{ comment.parent_id }}
+         {% endif %}
          {% if comment.type != 'U' %}comment-auto{% endif %}
          expanded">
 

commit d65f1c5ba3717ade33afd4ef4334e43a9daa4a3f

Allow for toggling instructions, fixed another regression from 3e088b4aeff687e72ac0d1fd8e2e00bdb3b45b74

review/models.py
@@ -35,6 +35,7 @@ class Comment(models.Model):
     def save(self, *args, **kwargs):
         super(Comment, self).save(*args, **kwargs)
         self.thread_id = self.parent_id or self.id
+        super(Comment, self).save(*args, **kwargs)
 
     #returns child and vote counts for child as a tuple
     def get_child_comment_vote(self):

review/views.py
@@ -65,7 +65,7 @@ def reply(request):
         if form.is_valid():
             comment = form.save(commit=False)
             comment.author = request.user
-            parent = Comment.objects.get(id=comment.parent.id)
+            parent = Comment.objects.get(id=comment.parent_id)
             comment.chunk = parent.chunk
             comment.end = parent.end
             comment.start = parent.start 

static/css/toolbar.css
@@ -7,11 +7,13 @@
     -webkit-box-sizing: border-box;
     -moz-box-sizing: border-box;
     -ms-box-sizing: border-box;
+    border-radius: 0px 0px 10px 10px ;
 }
 
 #toolbar-main {
     background: #eee;
     border: 1px solid #ccc;
+    border-top: 0px;
     box-sizing: border-box;
     -webkit-box-sizing: border-box;
     -moz-box-sizing: border-box;
@@ -61,7 +63,7 @@
 }
 
 #toolbar-instructions {
-    font-size: 1.2em;
+    font-size: 1em;
     background: #fafafa; 
     border: 1px solid #ccc;
     border-top: 0px;
@@ -70,5 +72,6 @@
     -moz-box-sizing: border-box;
     -ms-box-sizing: border-box;
     padding: 1em ;
+    border-radius: 0px 0px 10px 10px ;
 }
 

templates/chunks/view_chunk.html
@@ -318,7 +318,13 @@ $('#cancel-reply-button').live('click', function() {
     $('.reply-form').parent().remove();
 });
 
-
+var toggleInstructionsText = [ 'Hide instructions', 'Show instructions' ];
+var textIndex = 0;
+$('#toggle-instructions-button').click(function() {
+    $('#instructions-text').toggle(400);
+    textIndex = 1 - textIndex;
+    $(this).text(toggleInstructionsText[textIndex]);
+});
 
 });
 </script>
@@ -334,9 +340,14 @@ $('#cancel-reply-button').live('click', function() {
 {% endblock %}
 
 {% block instructions %}
-
 {% if task %}
-<p>Review this code. Please do at least one of the following:</p>
+<style type="text/css">
+#chunk-navigation {
+    float: right;
+}
+</style>
+<div id="instructions-text">
+<p>Please review this code by doing at least one of the following:</p>
 <ul>
   <li>Leave a comment on one or more lines of code.</li>
   <li>Reply to an existing comment.</li>
@@ -344,11 +355,16 @@ $('#cancel-reply-button').live('click', function() {
 </ul>
 <p>When you are done, please click the button, which will take you to the next
 function to review.</p>
+</div>
+<button id="toggle-instructions-button" type="button">Hide instructions</button>
+<div id="chunk-navigation">
 <form method="post" 
   action="{% url review.views.change_task %}?task_id={{ task.id }}&status=C">
   {% csrf_token %}
+N functions remaining for review
   <button id="done-button" type="submit">Next</button>
 </form>
+</div>
 
 {% endif %}
 {% endblock %}