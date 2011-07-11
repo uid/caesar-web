47e7a9b0bf0a5657a5f41400b92b9e9a93bc233d
Elena Tatarchenko <e.tatarchenko@gmail.com>
2011-07-10 22:54:31 -0400

    basic student view

--- chunks/urls.py
+++ chunks/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('chunks.views',
     (r'view/(?P<chunk_id>\d+)', 'view_chunk'),
+	(r'all/(?P<assign>(-|\w)+)/(?P<username>\w+)', 'view_all_chunks'),
 )
--- chunks/views.py
+++ chunks/views.py
@@ -1,4 +1,4 @@
-from chunks.models import Chunk, File
+from chunks.models import Chunk, File, Assignment
 from review.models import Comment, Vote, Star, Task
 
 from django.http import Http404
@@ -12,40 +12,46 @@ from pygments.formatters import HtmlFormatter
 
 @login_required
 def view_chunk(request, chunk_id):
-    user = request.user
-    chunk = get_object_or_404(Chunk, pk=chunk_id)
-    user_votes = dict((vote.comment_id, vote.value) \
-            for vote in user.votes.filter(comment__chunk=chunk_id))
+	user = request.user
+	chunk = get_object_or_404(Chunk, pk=chunk_id)
+	user_votes = dict((vote.comment_id, vote.value) \
+			for vote in user.votes.filter(comment__chunk=chunk_id))
 
-    def get_comment_data(comment):
-        vote = user_votes.get(comment.id, None)
-        snippet = chunk.generate_snippet(comment.start, comment.end)
-        return (comment, vote, snippet)
+	def get_comment_data(comment):
+		vote = user_votes.get(comment.id, None)
+		snippet = chunk.generate_snippet(comment.start, comment.end)
+		return (comment, vote, snippet)
 
-    comment_data = map(get_comment_data,
-            Comment.get_comments_for_chunk(chunk))
+	comment_data = map(get_comment_data,
+			Comment.get_comments_for_chunk(chunk))
 
-    lexer = JavaLexer()
-    formatter = HtmlFormatter(cssclass='syntax', nowrap=True)
-    numbers, lines = zip(*chunk.lines)
-    # highlight the code this way to correctly identify multi-line constructs
-    # TODO implement a custom formatter to do this instead
-    highlighted_lines = zip(numbers, 
-            highlight(chunk.data, lexer, formatter).splitlines())
-    
-    task_count = Task.objects.filter(reviewer=user.get_profile()) \
-            .exclude(status='C').count()
-    # get the associated task if it exists
-    try:
-        task = Task.objects.get(chunk=chunk, reviewer=user.get_profile())
-        if task.status == 'N':
-            task.mark_as('O')
-    except Task.DoesNotExist:
-        task = None
-    return render(request, 'chunks/view_chunk.html', { 
-        'chunk': chunk,
-        'highlighted_lines': highlighted_lines,
-        'comment_data': comment_data,
-        'task': task,
-        'task_count': task_count
-    }) 
+	lexer = JavaLexer()
+	formatter = HtmlFormatter(cssclass='syntax', nowrap=True)
+	numbers, lines = zip(*chunk.lines)
+	# highlight the code this way to correctly identify multi-line constructs
+	# TODO implement a custom formatter to do this instead
+	highlighted_lines = zip(numbers, 
+			highlight(chunk.data, lexer, formatter).splitlines())
+	
+	task_count = Task.objects.filter(reviewer=user.get_profile()) \
+			.exclude(status='C').count()
+	# get the associated task if it exists
+	try:
+		task = Task.objects.get(chunk=chunk, reviewer=user.get_profile())
+		if task.status == 'N':
+			task.mark_as('O')
+	except Task.DoesNotExist:
+		task = None
+	return render(request, 'chunks/view_chunk.html', { 
+		'chunk': chunk,
+		'highlighted_lines': highlighted_lines,
+		'comment_data': comment_data,
+		'task': task,
+		'task_count': task_count
+	}) 
+def view_all_chunks(request, assign, username):
+	chunks = Chunk.objects.filter(file__submission__name=username).filter(file__submission__assignment__name=assign)
+	
+	return render(request, 'chunks/view_all_chunks.html', {
+		'chunks': chunks
+	})
