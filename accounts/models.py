dd39d0b4c13c2895928d10b69b92c579d738e00d
Mason Tang <masont@mit.edu>
2011-07-22 17:57:39 -0400

    Added a reputation field in accounts.UserProfile, currently does nothing

--- accounts/models.py
+++ accounts/models.py
@@ -8,6 +8,7 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.conf import settings
+from django.dispatch import receiver
 
 class UserProfile(models.Model):
     def get_photo_path(instance, filename):
@@ -20,12 +21,13 @@ class UserProfile(models.Model):
     photo = ImageField(upload_to=get_photo_path)
     assigned_chunks = models.ManyToManyField(Chunk, through='tasks.Task',
         related_name='reviewers')
+    reputation = models.IntegerField(default=0, editable=False)
     
     def __unicode__(self):
         return self.user.__unicode__()
 
+
+@receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         profile, created = UserProfile.objects.get_or_create(user=instance)
-
-post_save.connect(create_user_profile, sender=User)
