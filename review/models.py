ac69b98034e3d8b1d82165a974043c3e59dc3b29
Mason Tang <masont@mit.edu>
2011-07-23 01:08:02 -0400

    Added reputation updates on vote update signals

--- review/models.py
+++ review/models.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.db.models import Count
+from django.db.models.signals import pre_save, post_save,\
+        pre_delete, post_delete
+from django.dispatch import receiver
+
 
 from accounts.models import UserProfile
 from chunks.models import Chunk
@@ -57,8 +61,6 @@ class Comment(models.Model):
     def get_comments_for_chunk(chunk):
         return chunk.comments.select_related('author')
 
-
-
     class Meta:
         ordering = [ 'start', '-end', 'thread_id', 'created' ]
 
@@ -72,10 +74,41 @@ class Vote(models.Model):
     author = models.ForeignKey(User, related_name='votes')
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+    
+    REPUTATION_WEIGHT = 1
+
+    def __unicode__(self):
+        return u'Vote(value=%s, comment=%s)' % (self.value, self.comment)
 
     class Meta:
         unique_together = ('comment', 'author',)
 
+
+@receiver(pre_save, sender=Vote)
+def update_reputation_on_vote_save(sender, instance, **kwargs):
+    comment_author = instance.comment.author.get_profile()
+    if instance.id:
+        old_vote = Vote.objects.get(pk=instance.id)
+        if old_vote.value > 0:
+            comment_author.reputation -= old_vote.value * Vote.REPUTATION_WEIGHT
+
+    new_value = int(instance.value)
+    if new_value > 0:
+        comment_author.reputation += new_value * Vote.REPUTATION_WEIGHT
+
+    comment_author.save()
+
+
+@receiver(pre_delete, sender=Vote)
+def update_reputation_on_vote_delete(sender, instance, **kwargs):
+    if instance.value > 0:
+        comment_author = instance.comment.author.get_profile()
+        comment_author.reputation -= instance.value * Vote.REPUTATION_WEIGHT
+        comment_author.save()
+
+
+@receiver(post_save, sender=Vote)
+@receiver(post_delete, sender=Vote)
 def denormalize_votes(sender, instance, created=False, **kwargs):
     """This recalculates the vote totals for the comment being voted on"""
     try:
@@ -87,8 +120,6 @@ def denormalize_votes(sender, instance, created=False, **kwargs):
         # vote is getting deleted from a comment delete cascade, do nothing
         pass
 
-models.signals.post_save.connect(denormalize_votes, sender=Vote)
-models.signals.post_delete.connect(denormalize_votes, sender=Vote)
 
 class Star(models.Model):
     value = models.BooleanField(default=False)
