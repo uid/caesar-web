0c0ce111559dbd6a9c1484a0bd3d07df45e9b55b
Mason Tang <masont@mit.edu>
2011-07-29 17:22:25 -0400

    Implemented first real iteration of a smarter task routing strategy

--- tasks/app_settings.py
+++ tasks/app_settings.py
@@ -1,4 +1,12 @@
 from django.conf import settings
 
-CHUNKS_PER_REVIEWER = getattr(settings, 'REVIEW_CHUNKS_PER_REVIEWER', 3)
-REVIEWERS_PER_CHUNK = getattr(settings, 'REVIEW_REVIEWERS_PER_CHUNK', 3)
+CHUNKS_PER_ROLE = getattr(settings, 'TASKS_CHUNKS_PER_ROLE', {
+    'student': 8,
+    'staff': 28,
+    'other': 5,
+})
+
+REVIEWERS_PER_CHUNK = getattr(settings, 'TASKS_REVIEWERS_PER_CHUNK', 2)
+
+ROLE_AFFINITY_MULTIPLIER = getattr(settings, 'TASKS_ROLE_AFFINITY_MULTIPLIER',
+                                   100)
--- tasks/routing.py
+++ tasks/routing.py
@@ -1,40 +1,197 @@
+from __future__ import division
+
+from collections import namedtuple, defaultdict
+import itertools
+
 from django.db.models import Count
 
-from chunks.models import Chunk
+from chunks import models
 from models import Task
 import app_settings
 
-def find_chunks(assignment, user):
-    """
-    Retrieves the chunks for this user to review on this assignment.
 
-    Does not assign them, this method simply retrieves chunk instances.
+def find_chunks(assignment, django_user):
     """
-    reviewer = user.get_profile()
-    current_task_count = Task.objects.filter(reviewer=reviewer, 
+    Computes the IDs of the chunks for this user to review on this assignment.
+
+    Does not assign them, this method simply retrieves chunk instances and
+    returns a generator of them.
+    """
+
+    # WARNING: These classes shadow the names of the actual model objects
+    # that they represent. This is deliberate, and the actual model classes 
+    # themselves are not reference in this function at all.
+
+    class User:
+        def __init__(self, id, role, reputation):
+            self.id = id
+            self.role = role
+            self.reputation = reputation
+            self.submissions = []
+            self.chunks = []
+            self.other_reviewers = set()
+
+        def __unicode__(self):
+            return u"User(id=%d, role=%s, reputation=%d)" % \
+                    (self.id, self.role, self.reputation)
+
+        def __str__(self):
+            return unicode(self).encode('utf-8')
+
+        def __repr__(self):
+            return str(self)
+
+        def __eq__(self, other):
+            return self.id == other.id
+
+        def __hash__(self):
+            return self.id
+
+    class Submission:
+        def __init__(self, submission, chunks):
+            self.id = submission['id']
+            self.reviewers = set()
+            self.chunks = (Chunk(chunk=chunk, submission=self) 
+                    for chunk in chunks)
+
+        def __str__(self):
+            return unicode(self).encode('utf-8')
+
+    class Chunk:
+        def __init__(self, chunk, submission):
+            self.id = chunk['id']
+            self.submission = submission
+            self.reviewers = set()
+        def assign_reviewer(self, user):
+            if user in self.reviewers:
+                return False
+
+            self.reviewers.add(user)
+            self.submission.reviewers.add(user)
+            user.chunks.append(self)
+
+            for reviewer in self.reviewers:
+                reviewer.other_reviewers.add(user)
+            user.other_reviewers.update(self.reviewers)
+
+    django_profile = django_user.get_profile()
+    user = User(
+            id=django_user.id,
+            role=django_profile.role,
+            reputation=django_profile.reputation)
+
+    current_task_count = Task.objects.filter(reviewer=django_profile, 
             chunk__file__submission__assignment=assignment).count()
     
-    assign_count = app_settings.CHUNKS_PER_REVIEWER - current_task_count
+    role = {'T': 'staff', 'S': 'student'}.get(django_profile.role, 'other')
+    assign_count = app_settings.CHUNKS_PER_ROLE[role] - current_task_count
     if assign_count <= 0:
-        return None
+        return
 
-    # Retrieve all remotely possible chunks
-    chunks = Chunk.objects.exclude(file__submission__author=user) \
+    chunks = []
+    submissions = {}
+    django_submissions = assignment.submissions \
+            .exclude(author=django_user).values().all()
+    django_chunks = models.Chunk.objects \
             .filter(file__submission__assignment=assignment) \
-            .exclude(tasks__reviewer=reviewer) 
+            .exclude(tasks__reviewer=django_profile) \
+            .values('id', 'file__submission')
 
-    # Annotate the chunks with counts
-    chunks = chunks.annotate(
-            reviewer_count=Count('tasks'),
-            submission_reviewer_count=Count(
-                'file__submission__files__chunks__tasks'))
+    django_submission_chunks = defaultdict(list)
+    for chunk in django_chunks:
+        django_submission_chunks[chunk['file__submission']].append(chunk)
 
-    # FIXME this query will probably need to be optimized
-    chunks = chunks \
-            .filter(reviewer_count__lt=app_settings.REVIEWERS_PER_CHUNK) \
-            .order_by('-reviewer_count')[0:assign_count]
+    # load all submissions and chunks into lightweight internal objects
+    for django_submission in django_submissions:
+        submission = Submission(submission=django_submission,
+                chunks=django_submission_chunks[django_submission['id']])
+        if not submission.chunks:
+            # toss out any submissions without chunks
+            continue
+        submissions[submission.id] = submission
+        chunks.extend(submission.chunks)
 
-    return chunks
+    # Sort the chunks according to these criteria:
+    # 
+    # For students and other:
+    #  1. Remove chunks already assigned to the user
+    #  2. Remove chunks with maximum number of reviewers
+    #  3. Find chunks with largest number of reviewers
+    #  4. Sort those chunks by number of reviewers assigned to submission,
+    #     which tries to distribute reviewers fairly among submissions.
+    #  5. Maximize affinity between user and reviewers on the submission,
+    #     which increases diversity of reviewers for submitter.
+    #  6. Maximize affinity between user and reviewers on the chunk, which
+    #     increases diversity of other reviewers for reviewer.
+    #
+    # For staff, we simply try to spread them out to maximize number of 
+    # submissions with at least one staff member, and then maximize the number
+    # of students that get to review a chunk along with staff.
+
+    def compute_affinity(user1, user2):
+        distance_affinity = 0
+        if user2 in user1.other_reviewers:
+            distance_affinity -= 50
+        
+        reputation_affinity = abs(user1.reputation - user2.reputation)
+
+        role_affinity = 0
+        role1, role2 = user1.role, user2.role
+        if role1 == 'student' and role2 == 'staff' or \
+                role1 == 'staff' and role2 == 'student':
+            role_affinity = 2
+        elif role1 == 'staff' and role2 == 'staff':
+            role_affinity = -100
+        else:
+            role_affinity = (role1 != role2)
+        role_affinity *= app_settings.ROLE_AFFINITY_MULTIPLIER
+
+        return distance_affinity + reputation_affinity + role_affinity
+
+    def make_chunk_sort_key(user):
+        def total_affinity(user, reviewers):
+            affinity = 0
+            for reviewer in reviewers:
+                affinity += compute_affinity(user, reviewer)
+            return affinity
+        if user.role == 'staff':
+            def chunk_sort_key(chunk):
+                return (
+                    -total_affinity(user, chunk.submission.reviewers),
+                    -total_affinity(user, chunk.reviewers),
+                )
+            return chunk_sort_key
+        else:
+            def chunk_sort_key(chunk):
+                return (
+                    user in chunk.reviewers,
+                    len(chunk.reviewers) >= app_settings.REVIEWERS_PER_CHUNK,
+                    -len(chunk.reviewers),
+                    len(chunk.submission.reviewers),
+                    -total_affinity(user, chunk.submission.reviewers),
+                    -total_affinity(user, chunk.reviewers),
+                )
+            return chunk_sort_key
+        
+    key = make_chunk_sort_key(user)
+
+    def assign_reviewer(chunk, reviewer):
+        if reviewer in chunk.reviewers:
+            return False
+
+        chunk.reviewers.add(reviewer)
+        chunk.submission.reviewers.add(reviewer)
+        reviewer.chunks.append(chunk)
+
+        for reviewer in chunk.reviewers:
+            reviewer.other_reviewers.add(reviewer)
+        reviewer.other_reviewers.update(chunk.reviewers)
+
+    for _ in itertools.repeat(None, assign_count):
+        # TODO consider using a priority queue here 
+        chunk_to_assign = min(chunks, key=key)
+        assign_reviewer(chunk_to_assign, user)
+        yield chunk_to_assign.id
 
 
 def assign_tasks(assignment, user):
@@ -43,13 +200,9 @@ def assign_tasks(assignment, user):
 
     Returns the number of chunks assigned.
     """
-    chunks = find_chunks(assignment, user)
-    if not chunks:
-        return 0
-    
     assign_count = 0
-    for chunk in chunks:
-        task = Task(reviewer=user.get_profile(), chunk=chunk)
+    for chunk_id in find_chunks(assignment, user):
+        task = Task(reviewer=user.get_profile(), chunk_id=chunk_id)
         task.save()
         assign_count += 1
         
