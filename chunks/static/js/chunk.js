ee2a802f6be0703cb6936ae4e21127c5ec023aba
Joe henke <jdhenke@mit.edu>
2012-04-05 15:06:08 -0400

    made hashtag highlighting accessible from both comments and wiki. some UI changes as well. #reviewthis elena_11@mit.edu

--- chunks/static/js/chunk.js
+++ chunks/static/js/chunk.js
@@ -353,33 +353,6 @@ function drawCommentButtons(comment) {
     });
 }
 
-// overrides onclick functionality of scrolling comments
-function hashtagRedirect(text) {
-    window.open(text, '_newtab');
-}
-
-function getFormattedHashtagText(text) {
-    var output = text.replace(/(#important[^\w])/ig, '<span class="hashtag-important">$1</span>');
-    output = output.replace(/(#magicnumber[^\w])/ig, '<a onclick=hashtagRedirect("http://c2.com/cgi/wiki?MagicNumber")>$1</span>');
-    output = output.replace(/(#namingconvention[^\w])/ig, '<a onclick=hashtagRedirect("http://www.oracle.com/technetwork/java/javase/documentation/codeconventions-135099.html#367")>$1</a>');
-    output = output.replace(/(#javadoc[^\w])/ig, '<a onclick=hashtagRedirect("http://www.oracle.com/technetwork/java/javase/documentation/index-137868.html")>$1</a>');
-    output = output.replace(/(#import[^\w])/ig, '<a onclick=hashtagRedirect("http://checkstyle.sourceforge.net/config_imports.html")>$1</a>');
-    output = output.replace(/(#braces[^\w])/ig, '<a onclick=hashtagRedirect("http://www.oracle.com/technetwork/java/javase/documentation/codeconventions-142311.html#430")>$1</a>');
-    output = output.replace(/(#modifierorder[^\w])/ig, '<a onclick=hashtagRedirect("http://checkstyle.sourceforge.net/config_modifier.html")>$1</a>');
-    output = output.replace(/(#size[^\w])/ig, '<a onclick=hashtagRedirect("http://checkstyle.sourceforge.net/config_sizes.html")>$1</a>');
-    output = output.replace(/(#innerassignment[^\w])/ig, '<a onclick=hashtagRedirect("http://checkstyle.sourceforge.net/config_coding.html")>$1</a>');
-    output = output.replace(/(#hashcode[^\w])/ig, '<a onclick=hashtagRedirect("http://www.technofundo.com/tech/java/equalhash.html")>$1</a>');
-    output = output.replace(/(#tab[^\w])/ig, '<a onclick=hashtagRedirect("http://geosoft.no/development/javastyle.html#Files")>$1</a>');
-    output = output.replace(/(#scope[^\w])/ig, '<a onclick=hashtagRedirect("http://geosoft.no/development/javastyle.html#Variables")>$1</a>');
-    return output;
-}
-
-function formatHashtags() {
-    $('.comment-text').each(function(i, l) {
-        l.innerHTML =  getFormattedHashtagText(l.textContent);
-    });
-}
-
 function clearSpecial() {
     $('.comment-text').removeClass('highlight');
     $('#voteup').removeClass('highlight');
