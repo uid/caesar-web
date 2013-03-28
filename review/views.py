a4478724df581b63cb6e83dac23b734f95f61da7
Christopher M Graves <graves@host.(none)>
2013-03-28 17:02:40 -0400

    Refactored the student stats into the admin

--- review/views.py
+++ review/views.py
@@ -127,41 +127,37 @@ def dashboard(request):
     })
 
 @staff_member_required
-def student_stats(request):
-    submit_milestones = SubmitMilestone.objects.all().order_by('-duedate')[0:5]
-    total_students = User.objects.filter(profile__role='S').count()
-    total_alum = User.objects.exclude(profile__role='S').exclude(profile__role='T').count()
-    all_alums = User.objects.exclude(profile__role='S').exclude(profile__role='T')
-    total_staff = User.objects.filter(profile__role='T').count()
+def review_milestone_info(request, review_milestone_id):
+    review_milestone = get_object_or_404(ReviewMilestone, pk=review_milestone_id)
+    total_chunks = Chunk.objects.filter(file__submission__milestone=review_milestone.submit_milestone).count()
+    alums_participating = User.objects.exclude(profile__role='S').exclude(profile__role='T')\
+                                    .filter(comments__chunk__file__submission__milestone=review_milestone.submit_milestone)\
+                                    .exclude(username='checkstyle').distinct().count()
+    total_tasks = Task.objects.filter(milestone=review_milestone).count()
+    assigned_chunks = Chunk.objects.filter(tasks__gt=0).filter(file__submission__milestone=review_milestone.submit_milestone).distinct().count()
+    total_chunks_with_human = Chunk.objects.filter(comments__type='U').filter(file__submission__milestone=review_milestone.submit_milestone).distinct().count()
+    total_comments = Comment.objects.filter(chunk__file__submission__milestone=review_milestone.submit_milestone).count()
+    total_checkstyle = Comment.objects.filter(chunk__file__submission__milestone=review_milestone.submit_milestone).filter(type='S').count()
+    total_staff_comments = Comment.objects.filter(chunk__file__submission__milestone=review_milestone.submit_milestone).filter(author__profile__role='T').count()
+    total_student_comments = Comment.objects.filter(chunk__file__submission__milestone=review_milestone.submit_milestone).filter(author__profile__role='S').count()
+    total_user_comments = Comment.objects.filter(chunk__file__submission__milestone=review_milestone.submit_milestone).filter(type='U').count()
+    total_alum_comments = total_user_comments - total_staff_comments - total_student_comments
+    zero_chunk_users = len(filter(lambda sub: len(sub.chunks()) == 0, review_milestone.submit_milestone.submissions.all()))
 
-    submit_milestone_data = []
-    for submit_milestone in submit_milestones:
-        total_chunks = Chunk.objects.filter(file__submission__milestone=submit_milestone).count()
-        all_alums = User.objects.exclude(profile__role='S').exclude(profile__role='T').filter(comments__chunk__file__submission__milestone=submit_milestone).exclude(username='checkstyle').distinct()
-        alums_participating = all_alums.count()
-        num_total_extensions = Extension.objects.filter(milestone=submit_milestone).count()
-        extension_data = []
-        for num_days in range(1, submit_milestone.max_extension+1):
-            num_extensions = Extension.objects.filter(milestone=submit_milestone).filter(slack_used=num_days).count()
-            extension_data.append((num_days, num_extensions))
-            
-        total_tasks = Task.objects.filter(milestone__submit_milestone=submit_milestone).count()
-        assigned_chunks = Chunk.objects.filter(tasks__gt=0).filter(file__submission__milestone=submit_milestone).distinct().count()
-        total_chunks_with_human = Chunk.objects.filter(comments__type='U').filter(file__submission__milestone=submit_milestone).distinct().count()
-        total_comments = Comment.objects.filter(chunk__file__submission__milestone=submit_milestone).count()
-        total_checkstyle = Comment.objects.filter(chunk__file__submission__milestone=submit_milestone).filter(type='S').count()
-        total_staff_comments = Comment.objects.filter(chunk__file__submission__milestone=submit_milestone).filter(author__profile__role='T').count()
-        total_student_comments = Comment.objects.filter(chunk__file__submission__milestone=submit_milestone).filter(author__profile__role='S').count()
-        total_user_comments = Comment.objects.filter(chunk__file__submission__milestone=submit_milestone).filter(type='U').count()
-        total_alum_comments = total_user_comments - total_staff_comments - total_student_comments
-        zero_chunk_users = len(filter(lambda sub: len(sub.files.all()) != 0 and len(sub.chunks()) == 0, submit_milestone.submissions.all()))
-        submit_milestone_data.append( (submit_milestone, total_chunks, alums_participating, num_total_extensions, extension_data, total_tasks, assigned_chunks, total_chunks_with_human, total_comments, total_checkstyle, total_alum_comments, total_staff_comments, total_student_comments, total_user_comments, zero_chunk_users) )
-
-    return render(request, 'review/studentstats.html', {
-        'submit_milestone_data': submit_milestone_data,
-        'total_students': total_students,
-        'total_alums': total_alum,
-        'total_staff': total_staff,
+    return render(request, 'review/review_milestone_info.html', {
+        'review_milestone': review_milestone,
+        'total_chunks': total_chunks,
+        'alums_participating': alums_participating,
+        'total_tasks': total_tasks,
+        'assigned_chunks': assigned_chunks,
+        'total_chunks_with_human': total_chunks_with_human,
+        'total_comments': total_comments,
+        'total_checkstyle': total_checkstyle,
+        'total_staff_comments': total_staff_comments,
+        'total_student_comments': total_student_comments,
+        'total_user_comments': total_user_comments,
+        'total_alum_comments': total_alum_comments,
+        'zero_chunk_users': zero_chunk_users,
     })
 
 @staff_member_required
