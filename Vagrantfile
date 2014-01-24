221eced898fb30ad0269830ac8335f3949cc4412
Rob Miller <rcm@mit.edu>
2014-01-23 23:15:07 -0500

    Moved sqlite database inside fixtures/ so that only two folders need to be writable by the web server (fixtures/ and media/).
    Fixed permissions on all files and folders so that they're all correct for Vagrant.
    
    Simplified Vagrantfile and setup script.

--- Vagrantfile
+++ Vagrantfile
@@ -11,8 +11,6 @@ Vagrant.configure("2") do |config|
   config.vm.network :private_network, ip: "10.18.6.30"
 
   config.vm.synced_folder "./", "/var/django/caesar",
-    owner: "caesar",
-    group: "dev",
-    mount_options: ["dmode=775,fmode=775"]
+    group: "www-data"
 
 end
--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+CAESAR_DIR=/var/django/caesar
+
 # Set time zone.
 #    **** comment out for now, not sure Caesar needs this
 #echo America/New_York > /etc/timezone
@@ -10,36 +12,26 @@
 /usr/bin/apt-get install -y apache2 apache2-dev libldap2-dev libsasl2-dev git libapache2-mod-wsgi python-dev python-numpy python-psycopg2 python-pip postfix 
 
 # Install Python packages we need.
-cd /var/django/caesar
-pip install -r requirements.txt
+cd /tmp
+pip install -r $CAESAR_DIR/requirements.txt
 
 # Make caesar user and group accounts.
 useradd -r caesar
-groupadd dev
-adduser caesar dev
-adduser vagrant dev
-adduser www-data dev
-
-# Set permissions on Caesar folder tree.
-#    **** hmm, chmod/chgrp/chown have no effect on the synced folder, so skip this for now 
-# cd /var/django/caesar
-# chgrp -R dev .
-# chown -R $USER .
-# chmod -R g+w .
-# chmod -R g+s .
+adduser caesar www-data
+adduser vagrant www-data
 
 # Make the media folder
-cd /var/django/caesar
-mkdir media && chmod g+w media
+mkdir $CAESAR_DIR/media
+
+# Set permissions on Caesar folder tree so that Apache server can write to the database and media folder.
+chgrp -R www-data $CAESAR_DIR/fixtures $CAESAR_DIR/media
 
 # Set up SSL, with MIT certificate authority for checking certificates presented by users.
-cp /var/django/caesar/apache/mitCAclient.pem /etc/ssl/certs/
-cd /etc/apache2/sites-enabled
-ln -s ../sites-available/default-ssl 000-default-ssl 
+cp $CAESAR_DIR/apache/mitCAclient.pem /etc/ssl/certs/
+ln -sf /etc/apache2/sites-available/default-ssl /etc/apache2/sites-enabled/000-default-ssl 
 a2enmod ssl
 
 # Install Caesar into Apache and start or restart Apache.
-ln -sf /var/django/caesar/apache/caesar /etc/apache2/sites-available
+ln -sf $CAESAR_DIR/apache/caesar /etc/apache2/sites-available
 a2ensite caesar
 sudo /etc/init.d/apache2 restart
-
