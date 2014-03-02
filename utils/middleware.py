5b65512d9b490680c53ee29268e83082df0a23ef
Rob Miller <rcm@mit.edu>
2014-03-02 15:46:50 -0500

    removed api/ and django-piston.  Closes #167.

--- utils/middleware.py
+++
@@ -1,9 +0,0 @@
-class ContentTypeMiddleware:
-    """
-    Workaround for a bug in django-piston
-    """
-    def process_request(self, request):
-        if request.method in ('POST', 'PUT'):
-            # dont break the multi-part headers !
-            if not 'boundary=' in request.META['CONTENT_TYPE']:
-                del request.META['CONTENT_TYPE']
