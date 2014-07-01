8dd591e1a904cbcdc4297d8427d74a0bb5b81a4e
kleinab <kleinab@mit.edu>
2014-07-01 15:30:26 -0400

    Now using cookies to store state of showing similar comments

--- sitestatic/js/comments_search.js
+++ sitestatic/js/comments_search.js
@@ -1,24 +1,28 @@
+var showSimilarCommentsState = $.cookie('showSimilarCommentsState') || 'expanded';
+
 function createSimilarCommentsDiv(comment_type) {
   var comment_header = $("<div class='comment-header'></span>");
   var comment_header_text = $("<span class='comment-header-text'>0 matching comments</span>")
   var comment_visibility = $("<span class='comment-visibility'></span>");
   comment_header.append(comment_visibility, comment_header_text);
   var similar_comments_wrapper = $("<div class='similar-comments-wrapper'></div>");
-  var similar_comments_display = $("<div class='similar-comments-display expanded "+comment_type+"'>");
+  var similar_comments_display = $("<div class='similar-comments-display "+showSimilarCommentsState+" "+comment_type+"'>");
   similar_comments_display.append(comment_header, similar_comments_wrapper);
   $(".new-"+comment_type).after(similar_comments_display);
 
   // Add listener to header, so user can collapse comment display
   $(comment_header).on("click", function() {
-    if ($(similar_comments_display).hasClass("expanded")) {
+    if ($(similar_comments_display).hasClass("expanded")) { // Collapse similar comments
+      $.cookie('showSimilarCommentsState', 'collapsed');
       $(similar_comments_wrapper).hide(effect="blind", complete=function() {
-        $(similar_comments_display).toggleClass("expanded");
-        $(similar_comments_display).toggleClass("collapsed");
+        $(similar_comments_display).removeClass("expanded");
+        $(similar_comments_display).addClass("collapsed");
       });
     }
-    else {
-      $(similar_comments_display).toggleClass("expanded");
-      $(similar_comments_display).toggleClass("collapsed");
+    else { // Expand similar comments
+      $.cookie('showSimilarCommentsState', 'expanded');
+      $(similar_comments_display).addClass("expanded");
+      $(similar_comments_display).removeClass("collapsed");
       $(similar_comments_wrapper).show(effect="blind");
     }
   });
