c1286565a17408afc58827c78a7a3fc1fb4626f0
kleinab <kleinab@mit.edu>
2014-07-07 10:08:14 -0400

    Use try catch to resolve error fullproof throws when only query words are stopwords.

--- sitestatic/js/comments_search.js
+++ sitestatic/js/comments_search.js
@@ -103,151 +103,161 @@ var commentSearch = new function() {
     var regex = new RegExp(pattern, "ig");
 
     // Request a search to the comments engine, then displays the results, if any.
-    commentsSearchEngine.lookup(value, function(resultset) {
-      var results = [];
-      if (resultset && resultset.getSize()) {
-        // resultset is a fullproof object that has its own forEach method
-        resultset.forEach(function(e) {
-          // Only choose scores that are "good enough"
-          if (e.score >= 2.5) {
-            results.push({
-              "index": e.value,
-              "score": e.score,
-              "bag_of_words": regex.exec(commentsData[e.value]),
-            });
+    try {
+      commentsSearchEngine.lookup(value, function(resultset) {
+        var results = [];
+        if (resultset && resultset.getSize()) {
+          // resultset is a fullproof object that has its own forEach method
+          resultset.forEach(function(e) {
+            // Only choose scores that are "good enough"
+            if (e.score >= 2.5) {
+              results.push({
+                "index": e.value,
+                "score": e.score,
+                "bag_of_words": regex.exec(commentsData[e.value]),
+              });
+            }
+          });
+        }
+
+        // Sort results from highest score to lowest score
+        results.sort(function(a,b) { return b.score - a.score; });
+
+        var ids = [];
+
+        // Display only the top 3 results.
+        // Use .html() rather than .text() to deal with special characters.
+        for (var i=0; i<Math.min(results.length, 3); i++) {
+
+          ids.push('#'+similarCommentClass+'-'+results[i].index);
+
+           // Check whether this result is already displayed
+          if ($('#'+similarCommentClass+'-'+results[i].index).length != 0) {
+            var comment_div = $('#'+similarCommentClass+'-'+results[i].index);
+            if (comment_div.index() != i) {
+              $('.similar-comments-wrapper > div:nth-child('+i+')').after(comment_div);
+              var text = $(comment_div).find(".comment-form .similar-comment-text").html();
+              $(comment_div).find(".comment-form .similar-comment-text").html(text.replace(regex, '<i><b>$&</b></i>'));
+              $(comment_div).hide();
+              $(comment_div).show("blind");
+            }
+            continue;
           }
-        });
-      }
 
-      // Sort results from highest score to lowest score
-      results.sort(function(a,b) { return b.score - a.score; });
+          var comment_div = $("<div class='comment "+similarCommentClass+" collapsed' id='"+similarCommentClass+"-"+results[i].index+"'></div>");
 
-      var ids = [];
+          // Link to comment in context
+          var comment_chunkdiv = $("<div class='comment-header'></div>");
 
-      // Display only the top 3 results.
-      // Use .html() rather than .text() to deal with special characters.
-      for (var i=0; i<Math.min(results.length, 3); i++) {
+          // TODO
+          var matches = [];
+          var found;
+          while (found = regex.exec(commentsData[results[i].index])) {
+            matches.push(found);
+          }
+          var comment_chunk_link = $("<a target='_blank'></a>");
 
-        ids.push('#'+similarCommentClass+'-'+results[i].index);
+          comment_chunk_link.attr("href", commentsExtraData[results[i].index].chunk_url);
+          comment_chunk_link.html(commentsExtraData[results[i].index].chunk_name);
+          comment_chunkdiv.append(comment_chunk_link);
 
-         // Check whether this result is already displayed
-        if ($('#'+similarCommentClass+'-'+results[i].index).length != 0) {
-          var comment_div = $('#'+similarCommentClass+'-'+results[i].index);
-          if (comment_div.index() != i) {
+          // Print the comment in a div
+          var comment_form = $("<div class='comment-form'></div>");
+          var comment_textdiv = $("<div class='"+similarCommentClass+"-text'></div>").html(commentsData[results[i].index].replace(regex, '<i><b>$&</b></i>'));
+          comment_form.append(comment_textdiv);
+
+          // Who wrote this comment and when?
+          var comment_author = $("<div class='comment-author'></div>");
+          var clipboard_button = $("<button class='clippy-button ui-button ui-widget ui-state-default ui-corner-all ui-button-icon-only' type='button' role='button' aria-disabled='false' title='Copy to clipboard'><span class='ui-button-icon-primary ui-icon ui-icon-clippy'></span><span class='ui-button-text'>Copy to clipboard</span></button>");
+          clipboard_button.attr("onclick", function() {
+            $(comment_textdiv).select();
+          });
+
+          comment_author.append(clipboard_button);
+          comment_author.append(commentsExtraData[results[i].index].date+" ago");
+          if (commentsExtraData[results[i].index].author != "") {
+            var author_link = $("<a target='_blank'></a>");
+            author_link.attr("href", commentsExtraData[results[i].index].author_url);
+            author_link.html(commentsExtraData[results[i].index].author);
+            comment_author.append(" by ", author_link);
+          }
+
+          // Link to comment in context
+          var comment_footer = $("<div class='comment-footer'></div>");
+          var chunk_link_button = $("<button class='comment-chunk-button ui-button ui-widget ui-state-default ui-corner-all ui-button-icon-only' type='button' role='button' aria-disabled='false' title='See context'><span class='ui-button-icon-primary ui-icon ui-icon-right-arrow'></span><span class='ui-button-text'>See context</span></button>");
+          chunk_link_button.attr("onclick", "window.open('"+commentsExtraData[results[i].index].chunk_url+"')");
+          comment_footer.append(chunk_link_button);
+
+          comment_div.append(comment_chunkdiv, comment_author, comment_form, comment_footer);
+
+          // Add new similar comment to after the previous result, in the correct order
+          if (i == 0) { // This is the first result to be displayed
+            $('.similar-comments-wrapper').prepend(comment_div);
+          }
+          else {
+            // jQuery is stupid and 1-indexes its selectors, thus using i rather i-1
             $('.similar-comments-wrapper > div:nth-child('+i+')').after(comment_div);
-            var text = $(comment_div).find(".comment-form .similar-comment-text").html();
-            $(comment_div).find(".comment-form .similar-comment-text").html(text.replace(regex, '<i><b>$&</b></i>'));
-            $(comment_div).hide();
-            $(comment_div).show("blind");
           }
-          continue;
+          $(comment_div).hide();
+          $(comment_div).show("blind");
+
+          $(comment_div).data("wasClicked", false);
+
+          // Listeners to appropriately expand and collapse comments when you hover or click on them
+          // When you hover over a comment, it should expand
+          // When you click on a comment header, it should stay expanded
+          $("#"+similarCommentClass+"-"+results[i].index+" .comment-header").on("click", function() {
+            var this_comment = $(this).parent();
+            var wasClicked = this_comment.data("wasClicked");
+            if (wasClicked && this_comment.hasClass("expanded")) {
+              this_comment.removeClass("expanded");
+              this_comment.addClass("collapsed");
+            }
+            else if (!wasClicked) {
+              this_comment.addClass("expanded");
+              this_comment.removeClass("collapsed");
+            }
+            this_comment.data("wasClicked", !wasClicked);
+          });
+
+          $(comment_div).on("mouseenter", function() {
+            var wasClicked = $(this).data("wasClicked");
+            if (!wasClicked && $(this).hasClass("collapsed")) {
+              $(this).addClass("expanded");
+              $(this).removeClass("collapsed");
+            }
+          });
+
+          $(comment_div).on("mouseleave", function() {
+            var wasClicked = $(this).data("wasClicked");
+            if (!wasClicked && $(this).hasClass("expanded")) {
+              $(this).removeClass("expanded");
+              $(this).addClass("collapsed");
+            }
+          });
+
         }
 
-        var comment_div = $("<div class='comment "+similarCommentClass+" collapsed' id='"+similarCommentClass+"-"+results[i].index+"'></div>");
+        // Remove results that weren't in the top 3
+        var selectorString = ".".concat(similarCommentClass)
+                                .concat(":not(")
+                                .concat(ids.join())
+                                .concat(")");
+        $(selectorString).remove();
 
-        // Link to comment in context
-        var comment_chunkdiv = $("<div class='comment-header'></div>");
+        // Update the number of matching comments, displayed in the header
+        $(".comment-header-text").text(ids.length+" matching comments");
 
-        // TODO
-        var matches = [];
-        var found;
-        while (found = regex.exec(commentsData[results[i].index])) {
-          matches.push(found);
-        }
-        var comment_chunk_link = $("<a target='_blank'></a>");
+      });
+    } catch(err) {
+      // fullproof engine throws an error when the only query words are stopwords (or the query is empty).
+      // This is ok because there will be 0 similar comments, so just hard code this.
 
-        comment_chunk_link.attr("href", commentsExtraData[results[i].index].chunk_url);
-        comment_chunk_link.html(commentsExtraData[results[i].index].chunk_name);
-        comment_chunkdiv.append(comment_chunk_link);
-
-        // Print the comment in a div
-        var comment_form = $("<div class='comment-form'></div>");
-        var comment_textdiv = $("<div class='"+similarCommentClass+"-text'></div>").html(commentsData[results[i].index].replace(regex, '<i><b>$&</b></i>'));
-        comment_form.append(comment_textdiv);
-
-        // Who wrote this comment and when?
-        var comment_author = $("<div class='comment-author'></div>");
-        var clipboard_button = $("<button class='clippy-button ui-button ui-widget ui-state-default ui-corner-all ui-button-icon-only' type='button' role='button' aria-disabled='false' title='Copy to clipboard'><span class='ui-button-icon-primary ui-icon ui-icon-clippy'></span><span class='ui-button-text'>Copy to clipboard</span></button>");
-        clipboard_button.attr("onclick", function() {
-          $(comment_textdiv).select();
-        });
-
-        comment_author.append(clipboard_button);
-        comment_author.append(commentsExtraData[results[i].index].date+" ago");
-        if (commentsExtraData[results[i].index].author != "") {
-          var author_link = $("<a target='_blank'></a>");
-          author_link.attr("href", commentsExtraData[results[i].index].author_url);
-          author_link.html(commentsExtraData[results[i].index].author);
-          comment_author.append(" by ", author_link);
-        }
-
-        // Link to comment in context
-        var comment_footer = $("<div class='comment-footer'></div>");
-        var chunk_link_button = $("<button class='comment-chunk-button ui-button ui-widget ui-state-default ui-corner-all ui-button-icon-only' type='button' role='button' aria-disabled='false' title='See context'><span class='ui-button-icon-primary ui-icon ui-icon-right-arrow'></span><span class='ui-button-text'>See context</span></button>");
-        chunk_link_button.attr("onclick", "window.open('"+commentsExtraData[results[i].index].chunk_url+"')");
-        comment_footer.append(chunk_link_button);
-
-        comment_div.append(comment_chunkdiv, comment_author, comment_form, comment_footer);
-
-        // Add new similar comment to after the previous result, in the correct order
-        if (i == 0) { // This is the first result to be displayed
-          $('.similar-comments-wrapper').prepend(comment_div);
-        }
-        else {
-          // jQuery is stupid and 1-indexes its selectors, thus using i rather i-1
-          $('.similar-comments-wrapper > div:nth-child('+i+')').after(comment_div);
-        }
-        $(comment_div).hide();
-        $(comment_div).show("blind");
-
-        $(comment_div).data("wasClicked", false);
-
-        // Listeners to appropriately expand and collapse comments when you hover or click on them
-        // When you hover over a comment, it should expand
-        // When you click on a comment header, it should stay expanded
-        $("#"+similarCommentClass+"-"+results[i].index+" .comment-header").on("click", function() {
-          var this_comment = $(this).parent();
-          var wasClicked = this_comment.data("wasClicked");
-          if (wasClicked && this_comment.hasClass("expanded")) {
-            this_comment.removeClass("expanded");
-            this_comment.addClass("collapsed");
-          }
-          else if (!wasClicked) {
-            this_comment.addClass("expanded");
-            this_comment.removeClass("collapsed");
-          }
-          this_comment.data("wasClicked", !wasClicked);
-        });
-
-        $(comment_div).on("mouseenter", function() {
-          var wasClicked = $(this).data("wasClicked");
-          if (!wasClicked && $(this).hasClass("collapsed")) {
-            $(this).addClass("expanded");
-            $(this).removeClass("collapsed");
-          }
-        });
-
-        $(comment_div).on("mouseleave", function() {
-          var wasClicked = $(this).data("wasClicked");
-          if (!wasClicked && $(this).hasClass("expanded")) {
-            $(this).removeClass("expanded");
-            $(this).addClass("collapsed");
-          }
-        });
-
-      }
-
-      // Remove results that weren't in the top 3
-      var selectorString = ".".concat(similarCommentClass)
-                              .concat(":not(")
-                              .concat(ids.join())
-                              .concat(")");
-      $(selectorString).remove();
-
+      $(".similar-comments-wrapper").empty();
       // Update the number of matching comments, displayed in the header
-      $(".comment-header-text").text(ids.length+" matching comments");
+      $(".comment-header-text").text("0 matching comments");
 
-    });
+    }
   };
 
 }
