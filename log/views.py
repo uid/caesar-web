0db6e8489bb2bdca9f9000198b4ac4cec527eb0d
kleinab <kleinab@mit.edu>
2014-11-24 12:15:28 -0500

    Logs are json rather than querydict, similar comments have size 1.2

--- log/views.py
+++ log/views.py
@@ -1,10 +1,11 @@
 import datetime
 from django.http import HttpResponse
+import json
 
 from log.models import Log
 
 def log(request):
     if request.is_ajax():
-      entry = Log(user=request.user, log=request.POST, timestamp=datetime.datetime.now())
+      entry = Log(user=request.user, log=json.dumps(request.POST), timestamp=datetime.datetime.now())
       entry.save()
     return HttpResponse()
