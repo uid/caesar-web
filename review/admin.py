8ba1384fa4f62904c6dc58bf6c26bbcd4881d4ef
Rob Miller <rcm@mit.edu>
2015-11-05 23:56:04 -0500

    a Chunk can now be safely opened in the admin view

--- review/admin.py
+++ review/admin.py
@@ -9,5 +9,6 @@ class CommentAdmin(admin.ModelAdmin):
     list_display = ('id', 'chunk', 'start', 'end', 'type', 'author', 'text')
     search_fields = ('chunk__name', 'text', 'author__username', 
             'author__first_name', 'author__last_name')
+    raw_id_fields = ('chunk', 'author', 'batch', 'parent', 'similar_comment')
 
 admin.site.register(Comment, CommentAdmin)
