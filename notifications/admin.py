8796c18aef23a708974ce053c990d06ceae86e15
Rob Miller <rcm@mit.edu>
2016-07-16 00:06:22 -0400

    moved Notifications to review app

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,10 +1,10 @@
-from django.contrib import admin
+# from django.contrib import admin
 
-from notifications.models import Notification
+# from notifications.models import Notification
 
-class NotificationAdmin(admin.ModelAdmin):
-    list_display = ('recipient', 'created', 'reason', 'comment', 'submission', 'email_sent')
-    search_fields = ('comment', 'submission', 'recipient')
+# class NotificationAdmin(admin.ModelAdmin):
+#     list_display = ('recipient', 'created', 'reason', 'comment', 'submission', 'email_sent')
+#     search_fields = ('comment', 'submission', 'recipient')
 
 
-admin.site.register(Notification, NotificationAdmin)
+# admin.site.register(Notification, NotificationAdmin)
--- review/admin.py
+++ review/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from review.models import Comment, Vote
+from review.models import Comment, Vote, Notification
 
 class VoteInline(admin.TabularInline):
     model = Vote
@@ -12,3 +12,10 @@ class CommentAdmin(admin.ModelAdmin):
     raw_id_fields = ('chunk', 'author', 'batch', 'parent', 'similar_comment')
 
 admin.site.register(Comment, CommentAdmin)
+
+
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('recipient', 'created', 'reason', 'comment', 'submission', 'email_sent')
+    search_fields = ('comment', 'submission', 'recipient')
+
+admin.site.register(Notification, NotificationAdmin)
