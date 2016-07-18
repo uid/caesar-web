777999662687b6235862b4dc2d533e4a8a83ec48
Rob Miller <rcm@mit.edu>
2016-07-18 02:31:20 -0400

    removed vestige of chunks app

--- chunks/forms.py
+++
@@ -1,6 +0,0 @@
-# from django import forms
-
-# class SimulateRoutingForm(forms.Form):
-#   num_students = forms.IntegerField()
-#   num_staff = forms.IntegerField()
-#   num_alum = forms.IntegerField()
--- chunks/templatetags/chunk_extras.py
+++
@@ -1,8 +0,0 @@
-# from django import template
-
-# register = template.Library()
-
-# def get_item(d, key):
-#   return d.get(key)
-
-# register.filter('get_item', get_item)
